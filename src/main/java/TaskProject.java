0320dc4f5713eeadb2cd36f0826a2a1395a51cfd
wuang <wuangmai@gmail.com>
2020-12-13 22:35:37 +0100

    menu działa, kolory są, czytanie zawartosci pliku do tablicy in progress

--- src/main/java/TaskProject.java
+++ src/main/java/TaskProject.java
@@ -1,17 +1,21 @@
-import java.io.Console;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class TaskProject {
     public static void main(String[] args) {
+        readTaskFile();
         menu();
     }
 
     public static void menu(){
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("TASK PROJECT v0.1");
+        System.out.println(ConsoleColors.PURPLE  + "TASK PROJECT v0.1" + ConsoleColors.RESET);
         System.out.println("add\nremove\nlist\nexit");
-        System.out.print("Please select an option: ");
+        System.out.print(ConsoleColors.BLUE + "Please select an option: ");
         String option = scan.nextLine();
 
             switch (option){
@@ -36,13 +40,39 @@ public class TaskProject {
                         System.out.print("\n\n");
                         menu();
             }
+    }
+
+    public static void readTaskFile(){
+        Path path = Paths.get("tasks.csv");
+        String[][] clearTab = new String[0][3];
+        int tabLength = -1;
 
 
+        try(Scanner scan = new Scanner(path)){
+            tabLength = addNewItemToMultiArray(clearTab,scan.nextLine().split(",")).length;
+            System.out.println(tabLength);
 
-
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
 
 
 
     }
+
+    public static String[][] addNewItemToMultiArray(String[][] array, String[] element) {
+        String[][] newArray = new String[array.length + 1][4];
+        for (int i = 0; i < newArray.length - 1; i++){
+            for (int j = 0; j < newArray[i].length; j++){
+                newArray[i][j] = array[i][j];
+            }
+        }
+        newArray[newArray.length - 1] = element;
+        return newArray;
+    }
+
+
+
+
 
 }
