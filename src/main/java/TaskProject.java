70335498333e429b549e3beb8bf26fbbd4192573
wuang <wuangmai@gmail.com>
2020-12-18 23:19:02 +0100

    taks array działa, list działa

--- src/main/java/TaskProject.java
+++ src/main/java/TaskProject.java
@@ -1,16 +1,14 @@
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
-import java.util.stream.Stream;
+
 
 public class TaskProject {
     public static void main(String[] args) {
-        readTaskFile();
         menu();
     }
 
@@ -20,6 +18,7 @@ public class TaskProject {
         System.out.println(ConsoleColors.PURPLE  + "TASK PROJECT v0.1" + ConsoleColors.RESET);
         System.out.println("add\nremove\nlist\nexit");
         System.out.print(ConsoleColors.BLUE + "Please select an option: ");
+        System.out.println(ConsoleColors.RESET);
         String option = scan.nextLine();
 
             switch (option){
@@ -33,6 +32,8 @@ public class TaskProject {
 
                 case "list":
                     System.out.println("Selected 'list'");
+                    list(taskArray());
+                    menu();
                     break;
 
                 case "exit":
@@ -46,29 +47,55 @@ public class TaskProject {
             }
     }
 
-    public static void readTaskFile(){
-        Path path = Paths.get("tasks.csv");
+    public static String[][] taskArray() {
         //sprawdza ile jest linii w pliku i tworzy tablice z taką ilością
+
+        Path path = Paths.get("tasks.csv");
         long numLines = 0;
-        try (Stream<String> stream = Files.lines(path, StandardCharsets.UTF_8)){
-            numLines = stream.count();
-        }catch (IOException ex){
+        try {
+            for (String line : Files.readAllLines(path)) {
+                numLines += 1;
+            }
+        } catch (IOException ex) {
             System.out.println("Critical error!");
         }
 
-        String[][] clearTab = new String[(int)numLines][3];
+        String[][] tab = new String[(int) numLines][3];
 
-        for (int i = 0; i < clearTab.length; i++){
-            for (int j = 0; j < clearTab[i].length; j++){
-                clearTab[i][j]=
+        // dodaje dane z pliku do multitablicy
+        File file = new File("tasks.csv");
+        try {
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()){
+                for (int i = 0; i < tab.length; i++) {
+                    String[] split = scan.nextLine().split(",");
+                    for (int j = 0; j < split.length; j++) {
+                        tab[i][j] = split[j];
+                    }
+                }
             }
+        }catch (FileNotFoundException ex){
+            ex.printStackTrace();
+        }
+        return tab;
+
+    }
+
+    public static void list(String[][] array){
+            StringBuilder sb = new StringBuilder();
+            sb.append("Nr\t Description \t \t \t Date \t is Important?\n");
+            for (int i = 0;i < array.length; i++){
+                sb.append(i).append(" : ");
+                for (int j = 0; j< array[i].length; j++){
+                    sb.append(array[i][j]);
+                }
+                sb.append("\n");
+            }
+            System.out.println(sb);
+
         }
 
 
-
-
-
-    }
 
     public static String[][] addNewItemToMultiArray(String[][] array, String[] element) {
         // coś w tym nie działa tak jak trzeba
